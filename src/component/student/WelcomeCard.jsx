00edc605ce4c299eca6e8e24c1c03881584580a0
Sanjay Kumar <sanjay.11801603@gmail.com>
2024-02-28 10:16:24 +0530

    Added the day Info along with clock

--- src/component/student/WelcomeCard.jsx
+++ src/component/student/WelcomeCard.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/student-dashboard.svg'
 const WelcomeCard = () => {
     const { userName, qoutesOfDay } = useContext(userContext)
     return (
-        <div className='student-card py-14 px-5 w-6/12 flex justify-between'>
+        <div className='student-card py-14 px-5 flex justify-between'>
             <div className='text-xl font-semibold w-9/12'>
                 Hi, Welcome back <span className='font-bold'>{userName}</span>👋
                 <p className='text-base font-light pe-8 mt-4'>{qoutesOfDay}</p>
