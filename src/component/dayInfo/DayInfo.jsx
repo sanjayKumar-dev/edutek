413741d5f615384778b916b53498f655bb7a5a04
Sanjay Kumar <sanjay.11801603@gmail.com>
2024-02-28 10:25:47 +0530

    Added day info

--- src/component/dayInfo/DayInfo.jsx
+++ src/component/dayInfo/DayInfo.jsx
@@ -2,23 +2,33 @@ import React, { useEffect, useState } from 'react'
 import './DayInfo.css'
 
 const DayInfo = () => {
-    const [currentTime, setCurrentTime] = useState(new Date());
+    const [currentDateTime, setCurrentDateTime] = useState(new Date())
 
     useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
+        const timerID = setInterval(() => tick(), 1000)
 
         return function cleanup() {
-            clearInterval(timerID);
-        };
-    });
+            clearInterval(timerID)
+        }
+    })
 
     function tick() {
-        setCurrentTime(new Date());
+        setCurrentDateTime(new Date())
     }
+
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        week: 'numeric'
+    }
+    const formattedDateTime = new Intl.DateTimeFormat('en-US', options).format(currentDateTime)
     return (
         <div className='dayinfo-container flex justify-center items-center'>
             <div>
-                <h1 className='text-3xl font-semibold'>{currentTime.toLocaleTimeString()}</h1>
+                <h1 className='text-3xl font-semibold'>{currentDateTime.toLocaleTimeString()}</h1>
+                <p className='mt-1 font-light'>{formattedDateTime}</p>
             </div>
         </div>
     )
