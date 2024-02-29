103330f14da5f90008d6f03c69bae539643b76e4
Sanjay Kumar <sanjay.11801603@gmail.com>
2024-02-29 23:17:47 +0530

    Made the footer font smaller

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 const Footer = () => {
     return (
-        <div className='footer-container flex justify-center'>
+        <div className='footer-container flex justify-center text-sm'>
             <li>Home</li>
             <li>About Us</li>
             <li>Contact Us</li>
